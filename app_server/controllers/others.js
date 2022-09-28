4c620cc6625ee1b73e3365f71acf3ada16b088f0
JonLuis.Koe <jonluis.koe@sckans.edu>
2022-09-27 20:17:02 -0500

    Commit to addingControllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,9 +1,8 @@
-/* GET home page. */
-// router.get('/', function(req, res) {
-//     res.render('index', { title: 'Tourism Website' });
-//   });
-
 // 'index' is index.jade
-module.exports.index = function(req, res){
-    res.render('index', { title: 'Tourism Website' });
+module.exports.home = function(req, res){
+    res.render('home', { title: 'Tourism Website' });
+};
+
+module.exports.aboutus = function(req, res){
+    res.render('aboutus', { title: 'About Us' });
 };
