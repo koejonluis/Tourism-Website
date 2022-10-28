bf73bc644602d188c442a5beb1828d6ff86fe8b5
JonLuis.Koe <jonluis.koe@sckans.edu>
2022-10-28 14:37:39 -0500

    Added Mongoose

---
+++ app_server/models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://localhost/TourismWebsite';
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+    
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
